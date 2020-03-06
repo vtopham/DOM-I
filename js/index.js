c849fa1fae8b10cb452e7530b1fc7f83986b42fe
Victoria  Topham <topham.victoria@gmail.com>
2020-03-05 16:35:10 -0800

    got the nav, cta, and the main content in the .js file

--- js/index.js
+++ js/index.js
@@ -40,3 +40,74 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+//Task 1: Create selectors to point your data into elements
+ //Create selectors by using any of the DOM element's methods
+ // Note that IDs have been used on all images. Use the IDs to update src path content
+
+
+
+//images
+let ctaImg = document.getElementById("cta-img"); 
+let mainImg = document.getElementById("middle-img");
+
+//nav
+let headerNav = document.querySelector("header nav"); 
+
+//CTA
+let ctaHeadline = document.querySelector(".cta-text h1"); 
+let ctaButton = document.querySelector(".cta-text button");
+
+//main content
+let mainH4 = document.querySelectorAll(".text-content h4");
+let mainText = document.querySelectorAll(".text-content p");
+
+//Task 2: Update the HTML with the JSON data
+ //Remember, NO direct updating of the HTML source is allowed.
+ //Using your selectors, update the content to match the example file.
+ //Remember to update the src attributes on images
+
+//update images
+ctaImg.src = siteContent["cta"]["img-src"];
+mainImg.src = siteContent["main-content"]["middle-img-src"];
+ 
+
+//header nav
+headerNav.children[0].text = siteContent["nav"]["nav-item-1"];
+headerNav.children[1].text = siteContent["nav"]["nav-item-2"];
+headerNav.children[2].text = siteContent["nav"]["nav-item-3"];
+headerNav.children[3].text = siteContent["nav"]["nav-item-4"];
+headerNav.children[4].text = siteContent["nav"]["nav-item-5"];
+headerNav.children[5].text = siteContent["nav"]["nav-item-6"];
+
+
+//CTA content
+ctaHeadline.textContent = siteContent["cta"]["h1"];
+ctaButton.textContent = siteContent["cta"]["button"];
+
+//main content
+
+mainH4[0].textContent = siteContent["main-content"]["features-h4"];
+mainH4[1].textContent = siteContent["main-content"]["about-h4"];
+mainH4[2].textContent = siteContent["main-content"]["services-h4"];
+mainH4[3].textContent = siteContent["main-content"]["product-h4"];
+mainH4[4].textContent = siteContent["main-content"]["vision-h4"];
+
+mainText[0].textContent = siteContent["main-content"]["features-content"];
+mainText[1].textContent = siteContent["main-content"]["about-content"];
+mainText[2].textContent = siteContent["main-content"]["services-content"];
+mainText[3].textContent = siteContent["main-content"]["product-content"];
+mainText[4].textContent = siteContent["main-content"]["vision-content"];
+
+
+
+
+
+
+
+//   "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+
+
+//   "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+// },
